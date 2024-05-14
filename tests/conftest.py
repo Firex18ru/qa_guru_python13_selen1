2cfca1f96fcaf4b1077bfc6a8f40e06fdb28b6ff
Firex18ru <Firex1@yandex.ru>
2024-05-14 21:46:17 +0400

    Правим ДЗ

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_managment():
+    browser.config.base_url = "https://demoqa.com/automation-practice-form"
+    browser.config.window_height = 768
+    browser.config.window_width = 1024
+    yield
+
+    browser.quit()
---
+++ tests/hw_selene.py
@@ -0,0 +1,8 @@
+from selene import browser, have, be
+
+
+def test_complete_todo():
+
+    browser.open("/")
+    browser.element()
+    browser.element
