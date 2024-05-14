5a331e0d4bfc41ee97040f9df056641679046350
Firex18ru <Firex1@yandex.ru>
2024-05-14 23:18:53 +0400

    Правим ДЗ

--- tests/hw_selene.py
+++ tests/hw_selene.py
@@ -4,5 +4,6 @@ from selene import browser, have, be
 def test_complete_todo():
 
     browser.open("/")
-    browser.element()
-    browser.element
+    browser.element("#firstName").type("Ivan").press_enter()
+    browser.element("#lastName").type("Yakimenko").press_enter()
+    browser.element("#userEmail-wrapper").tupe("Firex1@yandex.ru").press_enter()
