a421c73654e4d9df8ec88bf5beb0510f1d3d3cd5
Firex18ru <Firex1@yandex.ru>
2024-05-23 23:44:35 +0400

    продолжаю страдать над ДЗ =)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,12 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def browser_managment():
     browser.config.base_url = "https://demoqa.com"
     browser.config.window_height = 768
     browser.config.window_width = 1024
+    browser.config.timeout = 5
     yield
 
     browser.quit()
--- tests/hw_selene.py
+++ tests/hw_selene.py
@@ -1,13 +1,26 @@
 from selene import browser, have, be, by
 
 
-def test_demoqa():
-
+def test_demoqa_form():
+    # открываю регистрационную форму
     browser.open("/automation-practice-form")
+    # заполняю ФИО
     browser.element("#firstName").type("Ivan")
     browser.element("#lastName").type("Yakimenko")
-    browser.element("#userEmail-wrapper").type("Firex1@yandex.ru")
-
-    browser.element("#genterWrapper").all("label[for^=gender-radio]").element_by(
-        have.text("male")
+    # указываю адрес электронной почты
+    browser.element("#userEmail").type("Def11@def.ru")
+    # указываю пол
+    browser.element('[for="gender-radio-1"]').click()
+    # указываю номер контактного телефона
+    browser.element("#userNumber").type("+79997776019")
+    # указываю дату рождения
+    browser.element("#dateOfBirthInput").click()
+    browser.element(".react-datepicker__month-select").click().element(
+        "[value = '1']"
     ).click()
+    browser.element(".react-datepicker__year-select").click().element(
+        "[value = '1989']"
+    ).click()
+    browser.all(".react-datepicker__day--001").second.click()
+
+    browser.element("#subjectsInput").type("other")
