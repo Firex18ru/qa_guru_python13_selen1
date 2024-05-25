5e3551ebc23fceef594b75722515efbf54a5d7e8
Firex18ru <Firex1@yandex.ru>
2024-05-25 18:13:42 +0400

    итог

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope="session", autouse=True)
-def browser_managment():
+def browser_management():
     browser.config.base_url = "https://demoqa.com"
     browser.config.window_height = 1680
     browser.config.window_width = 1050
@@ -12,7 +12,7 @@ def browser_managment():
     options.page_load_strategy = "eager"
     driver = webdriver.Chrome(options=options)
     browser.config.driver = driver
-    # browser.config.timeout = 15
+
     yield
 
     browser.quit()
--- tests/test_hw_selene.py
+++ tests/test_hw_selene.py
@@ -4,23 +4,17 @@ import os
 
 def test_demoqa_form():
 
-    # открываю регистрационную форму
     browser.open("/automation-practice-form")
 
-    # заполняю ФИО
     browser.element("#firstName").type("Ivan")
     browser.element("#lastName").type("Yakimenko")
 
-    # указываю адрес электронной почты
     browser.element("#userEmail").type("Def11@def.ru")
 
-    # указываю пол
     browser.element('[for="gender-radio-1"]').click()
 
-    # указываю номер контактного телефона
     browser.element("#userNumber").type("0999777601")
 
-    # указываю дату рождения
     browser.element("#dateOfBirthInput").click()
     browser.element(".react-datepicker__month-select").click().element(
         "[value = '0']"
@@ -30,25 +24,18 @@ def test_demoqa_form():
     ).click()
     browser.all(".react-datepicker__day--001").second.click()
 
-    # указываю увлечения
     browser.element("#subjectsInput").type("Computer Science").press_enter()
     browser.element("[for = hobbies-checkbox-1]").click()
     browser.element("[for = hobbies-checkbox-2]").click()
 
-    # загружаю картинку
     browser.element("#uploadPicture").send_keys(os.path.abspath("./picture/094745.png"))
 
-    # указываю текущий адрес
     browser.element("#currentAddress").type("Izhevsk")
 
-    # указываю штат и город
     browser.element("#state").click().element("#react-select-3-option-0").click()
     browser.element("#city").click().element("#react-select-4-option-0").click()
 
-    # отправляю анкету
     browser.element("#submit").press_enter()
-
-    # проверка анкеты
 
     browser.element(".modal-content").element("table").all("tr").all("td").even.should(
         have.exact_texts(
