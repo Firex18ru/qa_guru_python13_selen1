ae7cf8582fabe7fea606be1fc6a61e052e703eb5
Firex18ru <Firex1@yandex.ru>
2024-05-25 15:36:26 +0400

    итог

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ from selenium import webdriver
 @pytest.fixture(scope="session", autouse=True)
 def browser_managment():
     browser.config.base_url = "https://demoqa.com"
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
+    browser.config.window_height = 1680
+    browser.config.window_width = 1050
     options = webdriver.ChromeOptions()
     options.page_load_strategy = "eager"
     driver = webdriver.Chrome(options=options)
